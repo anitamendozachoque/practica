18a3845f8edad1935d95d6d271ed39f2cee15ea3
anitamendozachoque <165625818+anitamendozachoque@users.noreply.github.com>
2024-04-07 22:56:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,21 @@ document.querySelector('#app').innerHTML = `
     <h2> HTML CSS & JS </h2>
     <a href="./src/hw01/q01.html"> Go to the first excercise </a>
     <a href="./src/hw02/q02.html"> Go to the second excercise </a>
-    <a href="./src/hw03/q03.html"> Go to the third excercise </a>
-  </div>
+    <a href="https://keybr.com"> Go to the third excercise </a>
+  
+    
+    </div>
+    const buyList = [
+      "books",
+      "bread",
+      "eraser",
+      "earphones",
+      "collection-cards",
+      "hdmi",
+      "vga",
+      "motherboard",
+      "university-books",
+    ];
 `
 
 setupCounter(document.querySelector('#counter'));
