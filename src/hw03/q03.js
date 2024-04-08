ad22c236f375c3d4f24fc1ceb3ac32ed90ef5a58
anitamendozachoque <165625818+anitamendozachoque@users.noreply.github.com>
2024-04-07 22:56:29 -0400

    Update q03.js

---
+++ src/hw03/q03.js
@@ -0,0 +1,25 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div style="display: flex; flex-direction: column;">
+    <h2> HTML CSS & JS </h2>
+    <a href="./src/hw01/q01.html"> Go to the first excercise </a>
+    <a href="./src/hw02/q02.html"> Go to the second excercise </a>
+    <a href="https://keybr.com"> Go to the third excercise </a>
+  
+    const buyList = [
+        "books",
+        "bread",
+        "eraser",
+        "earphones",
+        "collection-cards",
+        "hdmi",
+        "vga",
+        "motherboard",
+        "university-books",
+      ];
+    </div>
+    
+`
+
+setupCounter(document.querySelector('#counter'));
